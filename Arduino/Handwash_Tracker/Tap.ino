1f7d8fe80453f237270f9f6bd285a9cc1c84f9f3
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-03-27 12:14:32 +0530

    Update Tap.ino

--- Arduino/Handwash_Tracker/Tap.ino
+++ Arduino/Handwash_Tracker/Tap.ino
@@ -1,5 +1,5 @@
 int trigPin = 12;
-int echoPin = 13;
+int echoPin = 11;
 int Buzzer = 8;
 int duration, distance, dist;
 void setup() {
@@ -16,6 +16,10 @@ int checkDistance() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = duration*0.034/2;
+  Serial.print(duration);
+  Serial.print("\n");
+    Serial.print(distance);
+  Serial.print("\n");
   return distance;
 }
 
@@ -23,17 +27,19 @@ int checkDistance() {
 void loop() {
 
   dist = checkDistance();
-  if (distance <= 10)
+  if (distance <= 20)
   {
-    for(int i=0; i<=20; ++i) {
+    for(int i=0; ; ++i) {
       delay(1000);
       dist = checkDistance();
-      if(dist > 10){
+      if(dist > 20 && i<20){
         tone(Buzzer, 400);
         delay(2000);
         noTone(Buzzer);
         break;
       }
+      else if (dist > 20)
+        break;
     }
   }
   else {
